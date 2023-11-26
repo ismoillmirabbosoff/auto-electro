40005cd5bcf061830b6a512d79a0c33b55313ce7
Ismoil <ismoil@kali>
2023-11-27 01:22:15 +0500

    feat : feat fully configuration

---
+++ sendtelegram.js
@@ -0,0 +1,34 @@
+var telegram_bot_id = "6461470301:AAEGKbVRwnAylX60E7F31FfFVkhoH_t_8GY";
+//chat id
+var chat_id = -1002116842919;
+var u_name, email, message;
+var ready = function() {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nIzoh: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
